c6de4c1e4c1b45e00bde58bb1076ed5ae51f79a6
EC2 Default User <ec2-user@ip-172-31-34-67.eu-west-2.compute.internal>
2021-08-22 00:18:41 +0000

    Add compendium pack, update GM macro

--- scripts/gm_activityRequest.js
+++ scripts/gm_activityRequest.js
@@ -1,8 +1,13 @@
 const tokens = canvas.tokens.controlled.filter((t) => ['character', 'npc', 'familiar'].includes(t.actor.data.type));
 
 if (tokens.length === 0) {
-    ui.notifications.error(`You must select at least one npc/pc token`);
+    ui.notifications.error(`You must select at least one pc token`);
 } else {
+    console.log(game);
+    let chatData = { 
+        content: "GM has requested Exploration Activities", 
+    }; 
+    ChatMessage.create(chatData, {}); 
     tokens.map((p) => p.actor).forEach((actor) => 
     game.socket.emit('module.pf2e-explorationActivities', {
       operation: 'playerExplorationActivity',
